8ee1be2c55545f252c04c72b022b4513aa83d734
tqcenglish <tqcenglish@gmail.com>
2020-07-24 12:00:05 +0800

    update read max

--- backup/index.js
+++ backup/index.js
@@ -2,7 +2,7 @@ const dataDownload = document.getElementById("data_download");
 dataDownload.addEventListener("click", () => {
     cockpit.script("/root/.local/share/cockpit/backup/bin/back_db.sh").stream(() => {})
         .then(() => {
-            return cockpit.file('/tmp/sql.sql').read().then((data) => {
+            return cockpit.file('/tmp/sql.sql',{max_read_size: 100*1024*1024}).read().then((data) => {
                 let blob = new Blob([data], { type: "text/plain;charset=utf-8" });
                 saveAs(blob, "coopaging.sql")
             });
@@ -40,10 +40,10 @@ uploadFile.addEventListener('change', function () {
 
 const logDownload = document.getElementById("log_download");
 logDownload.addEventListener('click', () => {
-    return cockpit.file('/var/log/message').read().then((data) => {
+    return cockpit.file('/var/log/messages',{max_read_size: 100*1024*1024}).read().then((data) => {
         let blob = new Blob([data], { type: "text/plain;charset=utf-8" });
-        saveAs(blob, "message.log")
-    });
+        saveAs(blob, "messages.log")
+    }).catch(err => console.error(err));
 })
 
 // Send a 'init' message.  This tells integration tests that we are ready to go
