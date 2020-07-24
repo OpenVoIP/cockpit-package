b1b32a0904a426e2b58766e5aac18f7b331beadb
juncheng.du <juncheng.du@zycoo.com>
2020-07-24 11:23:03 +0800

    add import_db.sh

---
+++ backup/bin/import_db.sh
@@ -0,0 +1,2 @@
+docker exec  zycoo_mysql mysql -ucoopaging -pZycoo_coopaging_666 coopaging_db < /tmp/sql.sql
+exit 0
--- backup/index.js
+++ backup/index.js
@@ -1,24 +1,22 @@
 const dataDownload = document.getElementById("data_download");
 dataDownload.addEventListener("click", () => {
-    cockpit.script("/root/.local/share/cockpit/backup/bin/back_db.sh").stream(()=>{
-       
-    })
-    .then(()=>{
-     return cockpit.file('/tmp/sql.sql').read().then((data) => {
-          let blob = new Blob([data], { type: "text/plain;charset=utf-8" });
-          saveAs(blob, "coopaging.sql")
-   	});
-    })
-    .catch(err => {
-        console.error(err);
-    });
+    cockpit.script("/root/.local/share/cockpit/backup/bin/back_db.sh").stream(() => {})
+        .then(() => {
+            return cockpit.file('/tmp/sql.sql').read().then((data) => {
+                let blob = new Blob([data], { type: "text/plain;charset=utf-8" });
+                saveAs(blob, "coopaging.sql")
+            });
+        })
+        .catch(err => {
+            console.error(err);
+        });
 });
 
 const dataUpload = document.getElementById("data_upload");
 let uploadData;
 dataUpload.addEventListener("click", () => {
-    cockpit.file('/tmp/sql.sql').replace(uploadData).then((data) => {
-        console.log(data);
+    cockpit.file('/tmp/sql.sql').replace(uploadData).then(() => {
+        return cockpit.script("/root/.local/share/cockpit/backup/bin/import_db.sh").stream(() => {});
     }).catch(err => {
         console.error(err);
     });
@@ -26,22 +24,26 @@ dataUpload.addEventListener("click", () => {
 
 
 let uploadFile = document.getElementById('upload_file');
-uploadFile.addEventListener('change', function() {
+uploadFile.addEventListener('change', function () {
     let reads = new FileReader();
     let file = this.files[0];
+    console.log(file.name);
+    if(!file.name || !file.name.endsWith('.sql')){
+        return alert(`${file.name} not is sql file`);
+    }
     reads.readAsText(file, 'utf-8');
     reads.onload = function (e) {
         uploadData = e.target.result;
     };
-    console.log(file.name);
 }, false);
+
 
 const logDownload = document.getElementById("log_download");
 logDownload.addEventListener('click', () => {
-    cockpit.spawn(["ping", "-c", "4", address.value])
-        .stream(ping_output)
-        .then(ping_success)
-        .catch(ping_fail);
+    return cockpit.file('/var/log/message').read().then((data) => {
+        let blob = new Blob([data], { type: "text/plain;charset=utf-8" });
+        saveAs(blob, "message.log")
+    });
 })
 
 // Send a 'init' message.  This tells integration tests that we are ready to go
